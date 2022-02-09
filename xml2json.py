485bf475d79658586f6052fb7a953d25e1389298
javi <javigr6623@gmail.com>
2022-02-09 18:31:28 +0100

    def created

--- xml2json.py
+++ xml2json.py
@@ -2,35 +2,48 @@ import json
 import xmltodict
 from mapp import mapping_xml
 
-with open('xml-entry-sample.xml', 'r') as file:
-    obj = xmltodict.parse(file.read())
-    # TODO: PROBLEMA CON LOS ACENTOS
-    obj_str = json.dumps(obj)
-    obj_json = json.loads(obj_str)['entry']
-    file.close()
 
-print("FUTURE MAPPING:")
-first_value_to_map = {}
-cac_value_to_map = []
-# TODO: TODOS LOS CAC TIENE MAS DENTRO
-for x in obj_json:
-    if x == 'summary':
-        first_value_to_map[x] = obj_json[x]['#text']
-    elif x == 'link':
-        first_value_to_map[x] = obj_json[x]['@href']
-    elif x == 'cac-place-ext:ContractFolderStatus':
-        print('\n')
-    else:
-        first_value_to_map[x] = obj_json[x]
+def read_xml_file(filename):
+    with open(filename, 'r') as file:
+        obj = xmltodict.parse(file.read())
+        # TODO: PROBLEMA CON LOS ACENTOS
+        obj_str = json.dumps(obj)
+        obj_json = json.loads(obj_str)['entry']
+        file.close()
+        return obj_json
 
-for key, value in mapping_xml.items():
-    first_value_to_map[value] = first_value_to_map.pop(key)
 
-# EXTRAS
-first_value_to_map['bidding'] = first_value_to_map['short_description'].split(":")[1].split(";")[0]
-first_value_to_map['contracting_authority'] = first_value_to_map['short_description'].split(":")[2].split(";")[0]
-first_value_to_map['cost'] = first_value_to_map['short_description'].split(":")[3].split(";")[0]
-first_value_to_map['status'] = first_value_to_map['short_description'].split(":")[4].split(";")[0]
+def first_mapping_extract(json_extract):
+    first_value_to_map = {}
+    # cac_value_to_map = []
+    # TODO: TODOS LOS CAC TIENE MAS DENTRO
+    for x in json_extract:
+        if x == 'summary':
+            first_value_to_map[x] = json_extract[x]['#text']
+        elif x == 'link':
+            first_value_to_map[x] = json_extract[x]['@href']
+        elif x == 'cac-place-ext:ContractFolderStatus':
+            print('\n')
+        else:
+            first_value_to_map[x] = json_extract[x]
 
-print(first_value_to_map)
+    for key, value in mapping_xml.items():
+        first_value_to_map[value] = first_value_to_map.pop(key)
+    return first_value_to_map
+
+
+def extra_mapping_extract(first_mapp):
+    mapp_return = first_mapp
+    mapp_return['bidding'] = first_mapp['short_description'].split(":")[1].split(";")[0]
+    mapp_return['contracting_authority'] = first_mapp['short_description'].split(":")[2].split(";")[0]
+    mapp_return['cost'] = first_mapp['short_description'].split(":")[3].split(";")[0]
+    mapp_return['status'] = first_mapp['short_description'].split(":")[4].split(";")[0]
+    return mapp_return
+
+
+xml_json = read_xml_file('xml-entry-sample.xml')
+print("MAPPING...")
+first_mapping = first_mapping_extract(xml_json)
+extra_mapping = extra_mapping_extract(first_mapping)
+print(extra_mapping)
 print("---------------------")
